e8c359599df0179b72467d9c4a4e7d60c78ddcc8
Hyrum Draughon <hyrumdraughon@gmail.com>
2021-03-04 08:52:18 -0700

    Solution

--- src/main/java/com/cooksys/ftd/assignments/objects/Rational.java
+++ src/main/java/com/cooksys/ftd/assignments/objects/Rational.java
@@ -1,75 +1,103 @@
 package com.cooksys.ftd.assignments.objects;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 public class Rational implements IRational {
-    /**
-     * Constructor for rational values of the type:
-     * <p>
-     * `numerator / denominator`
-     * <p>
-     * No simplification of the numerator/denominator pair should occur in this method.
-     *
-     * @param numerator   the numerator of the rational value
-     * @param denominator the denominator of the rational value
-     * @throws IllegalArgumentException if the given denominator is 0
-     */
-    public Rational(int numerator, int denominator) throws IllegalArgumentException {
-        throw new NotImplementedException();
-    }
 
-    /**
-     * @return the numerator of this rational number
-     */
-    @Override
-    public int getNumerator() {
-        throw new NotImplementedException();
-    }
+	private int numerator;
+	private int denominator;
 
-    /**
-     * @return the denominator of this rational number
-     */
-    @Override
-    public int getDenominator() {
-        throw new NotImplementedException();
-    }
+	/**
+	 * Constructor for rational values of the type:
+	 * <p>
+	 * `numerator / denominator`
+	 * <p>
+	 * No simplification of the numerator/denominator pair should occur in this
+	 * method.
+	 *
+	 * @param numerator   the numerator of the rational value
+	 * @param denominator the denominator of the rational value
+	 * @throws IllegalArgumentException if the given denominator is 0
+	 */
+	public Rational(int numerator, int denominator) throws IllegalArgumentException {
+		if (denominator == 0) {
+			throw new IllegalArgumentException();
+		}
 
-    /**
-     * Specializable constructor to take advantage of shared code between Rational and SimplifiedRational
-     * <p>
-     * Essentially, this method allows us to implement most of IRational methods directly in the interface while
-     * preserving the underlying type
-     *
-     * @param numerator the numerator of the rational value to construct
-     * @param denominator the denominator of the rational value to construct
-     * @return the constructed rational value
-     * @throws IllegalArgumentException if the given denominator is 0
-     */
-    @Override
-    public Rational construct(int numerator, int denominator) throws IllegalArgumentException {
-        throw new NotImplementedException();
-    }
+		this.numerator = numerator;
+		this.denominator = denominator;
+	}
 
-    /**
-     * @param obj the object to check this against for equality
-     * @return true if the given obj is a rational value and its
-     * numerator and denominator are equal to this rational value's numerator and denominator,
-     * false otherwise
-     */
-    @Override
-    public boolean equals(Object obj) {
-        throw new NotImplementedException();
-    }
+	/**
+	 * @return the numerator of this rational number
+	 */
+	@Override
+	public int getNumerator() {
+		return numerator;
+	}
 
-    /**
-     * If this is positive, the string should be of the form `numerator/denominator`
-     * <p>
-     * If this is negative, the string should be of the form `-numerator/denominator`
-     *
-     * @return a string representation of this rational value
-     */
-    @Override
-    public String toString() {
-        throw new NotImplementedException();
-    }
+	/**
+	 * @return the denominator of this rational number
+	 */
+	@Override
+	public int getDenominator() {
+		return denominator;
+	}
+
+	/**
+	 * Specializable constructor to take advantage of shared code between Rational
+	 * and SimplifiedRational
+	 * <p>
+	 * Essentially, this method allows us to implement most of IRational methods
+	 * directly in the interface while preserving the underlying type
+	 *
+	 * @param numerator   the numerator of the rational value to construct
+	 * @param denominator the denominator of the rational value to construct
+	 * @return the constructed rational value
+	 * @throws IllegalArgumentException if the given denominator is 0
+	 */
+	@Override
+	public Rational construct(int numerator, int denominator) throws IllegalArgumentException {
+		if (denominator == 0) {
+			throw new IllegalArgumentException();
+		}
+
+		return new Rational(numerator, denominator);
+	}
+
+	/**
+	 * @param obj the object to check this against for equality
+	 * @return true if the given obj is a rational value and its numerator and
+	 *         denominator are equal to this rational value's numerator and
+	 *         denominator, false otherwise
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Rational other = (Rational) obj;
+		if (numerator != other.getNumerator() || denominator != other.getDenominator()) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * If this is positive, the string should be of the form `numerator/denominator`
+	 * <p>
+	 * If this is negative, the string should be of the form
+	 * `-numerator/denominator`
+	 *
+	 * @return a string representation of this rational value
+	 */
+	@Override
+	public String toString() {
+		if (numerator < 0 != denominator < 0) {
+			return "-" + Math.abs(numerator) + "/" + Math.abs(denominator);
+		} else {
+			return Math.abs(numerator) + "/" + Math.abs(denominator);
+		}
+	}
 }
